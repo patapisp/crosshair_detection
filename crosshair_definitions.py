e46b30721a0c5c2a21371f7c73f61e5fcc734a56
binkertf <41291775+binkertf@users.noreply.github.com>
2018-07-24 17:09:09 +0200

    Add files via upload

--- crosshair_definitions.py
+++ crosshair_definitions.py
@@ -9,7 +9,6 @@ from skimage.color import rgb2gray
 from astropy.convolution import Gaussian2DKernel, convolve
 
 
-
 gauss_response = lambda x, x0, sigma, A: A*0.5*(1 + erf((x-x0)/(sigma**2*np.sqrt(2))))
 gauss_response_inv = lambda x, x0, sigma, A: 1*A-gauss_response(x, x0, sigma, A)
 
@@ -249,3 +248,248 @@ def find_center2D(data_in, kernel_size):
     edges_averaged =  scipy.signal.convolve2d(edges, kernel, 'valid')
     img_conv = convolve(edges_averaged,CrossHairKernel2D(71,25))
     plt.imshow(img_conv, interpolation='nearest')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+###############################################################################
+'''
+takes a line in Hesse-form (r,theta) and gives back two points on the line in 
+xy-coordinates outside of the image borders
+INPUT
+radius:radius of Hesse-form
+theta: angle theta of Hesse-form
+OUTPUT
+x1: x-coordinate of first point
+y1: y-coordinate of first point
+x2: x-coordinate of second point
+y2: y-coordinate of second point
+'''
+###############################################################################
+def hesse_to_xy(radius,theta):
+    ln = 6000
+    if abs(theta) < 1e-4: #avoid theta=0
+        x1 = int(radius)
+        y1 = ln
+        x2 = int(radius)
+        y2 = -ln   
+    else:
+        m = -1.0/np.tan(theta)
+        b = radius/np.sin(theta)
+        x1=ln
+        y1 = int(m*x1+b)
+        x2 = -1*int(ln)
+        y2 = int(m*x2+b)
+    return ((x1),(y1)),((x2),(y2))
+
+###############################################################################
+'''
+takes an array of lines and finds the 'number_of_lines' best fitting lines
+INPUT
+lines: nx2 array of n lines in Hesse normal form
+number_of_lines: number of lines which are needed (4 per cross)
+space: two lines need to have more than 'space' pixels distance from each other
+to be considered 
+OUTPUT
+valid_lines: number_of_linesx2 array of lines in Hesse normal form
+'''
+###############################################################################
+def find_fittest_lines4(lines,number_of_lines,ds,image):
+    valid_lines = np.zeros_like(lines[0:number_of_lines,:]) #container for valid lines
+    if lines[0,0]<0:
+        valid_lines[0,0]=abs(lines[0,0])
+        valid_lines[0,1]=lines[0,1]-np.pi     #first entry
+    else:
+        valid_lines[0,:]=lines[0,:]
+    j= 0 #runs through 'lines'
+    for i in range(number_of_lines-1):
+        valid_lines[i+1]=i+1
+        count = 1    #abbort criterion for while loop
+        while count == 1:
+            count = 0 #reset abort criterion
+            j+=1
+            take = lines[j,:]
+            if take[0]<0:
+                take[0] = abs(take[0])
+                take[1] = take[1]-np.pi
+            for k in range(number_of_lines): #take line j and check if within 'space' pixels of line k
+                if  lines_are_close(take,valid_lines[k,:],ds,image):
+                    count = 1#if line j is too close, run loop again with line j=j+1
+        valid_lines[i+1,:]=take   #keep line j if it has nececarry distance 
+    return valid_lines
+
+###############################################################################
+'''
+takes an array of lines in Hesse normal form and sorts according to angle 
+(smallest angle first)
+INPUT
+lines_input: nx2 array of n lines in Hesse normal form (unsorted)
+OUTPUT
+sorted_lines: nx2 array of n lines sorted according to angle
+'''
+###############################################################################
+def sort_lines(lines_input):
+    for i in range(lines_input.shape[0]): #check for negative radii
+        if lines_input[i,0]<0:
+            lines_input[i,0]=abs(lines_input[i,0])
+            lines_input[i,1]=lines_input[i,1]-np.pi
+    sorted_lines = np.zeros_like(lines_input)
+    sort = np.argsort(lines_input[:,1])
+    for i in range(lines_input.shape[0]):
+        sorted_lines[i]=lines_input[sort[i]]
+    return sorted_lines
+
+###############################################################################
+'''
+finds center coordinate of FOUR lines
+INPUT
+sorted_lines:4x2 array of lines in Hesse normal form, sorted according to angle 
+(smallest angle first)
+OUTPUT
+xy coordinate of center'''
+###############################################################################
+def find_center(sorted_lines):
+    a = np.array([1.1,1.1])
+    a[0]=(0.5*(sorted_lines[0,0]+sorted_lines[1,0]))
+    a[1]=(0.5*(sorted_lines[0,1]+sorted_lines[1,1]))
+    b = np.array([0.0,0.0])
+    b[0]=(0.5*(sorted_lines[2,0]+sorted_lines[3,0]))
+    b[1]=(0.5*(sorted_lines[2,1]+sorted_lines[3,1]))
+    r_a = a[0]
+    th_a = a[1]+1e-6    #### avoid th_a=0
+    r_b = b[0]
+    th_b = b[1]+1e-6    #### avoid th_b=0
+    cx = 1.0/(np.tan(th_a)-np.tan(th_b))*(r_b*np.sin(th_a)-r_a*np.sin(th_b))/(np.cos(th_b)*np.cos(th_a))
+    cy = 1.0/np.sin(th_a)*(r_a-cx*np.cos(th_a))
+    return cx,cy
+
+###############################################################################
+'''
+performs Hough Line Transform
+INPUT
+edges: image containing contours
+r_res: radial resolution of HLT
+theta_res: angular resolution of HLT
+v_threshold: only lines with vote above 'v_threshold' are considered
+OUTPUT
+lines: nx2 array of all the lines with at least a vote of 'v_treshold'
+'''
+###############################################################################
+def do_HoughLines(edges,r_res,theta_res,v_threshold):
+    lines1 = cv2.HoughLines(edges,r_res,theta_res,v_threshold)
+    lines = lines1[:,0,:]
+    return lines
+
+###############################################################################
+'''
+determines whether two lines represent the same edge
+INPUT
+
+OUTPUT
+True: the two lines are within 'ds' pixels of each other
+False: the two lines are farther apart
+'''
+###############################################################################
+def lines_are_close(line_1,line_2,ds,image):
+    if abs(line_1[0]-line_2[0]) < ds:
+        return True
+    else:
+        return False
+    
+###############################################################################
+'''
+converts the image to a binarey image to be fed to the HLT algorithm
+INPUT
+im: nxmx3 array, RGB image
+thresh: threshold value between black and white
+OUTPUT
+edges: binary image as nxmx3 array
+'''
+###############################################################################
+def rgb2edges_Canny(im,thresh):
+    im2 = np.zeros_like(im)
+    grey_int255 = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY )
+    grey_int255[grey_int255>thresh]=255
+    grey_int255[grey_int255<=thresh]=0
+    im2[:,:,0]=grey_int255
+    im2[:,:,1]=grey_int255
+    im2[:,:,2]=grey_int255
+    edges = cv2.Canny(im2,80,70)
+    return edges
+
+###############################################################################
+'''
+draws lines
+INPUT
+nx2 array of n lines in Hesse normal form 
+'''
+###############################################################################
+def drawlines(lines_array,image,thickness):
+    for i in range(lines_array.shape[0]):
+        p1,p2 = hesse_to_xy(lines_array[i,0],lines_array[i,1])
+        cv2.line(image,p1,p2,(0,0,255),thickness)
+    return 0
+###############################################################################
+'''
+splits an 8x2 array of lines in Hesse normal for into two 4x2 arrays sorted by angle
+INPUT
+8x2 array of 8 lines in Hesse normal form in format (radius, theta)
+OUTPUT
+chair1:4x2 array of four lines in Hesse normal form (contains line with smallest angle)
+chair2: 4x2 array of four lines in Hesse normal form (contains line with largest angle)
+'''
+###############################################################################
+def separate_crosshairs(lines):
+    nr = int(lines.shape[0]/4)
+    chair1 = np.zeros_like(lines[0:2*nr])
+    chair2 = np.zeros_like(lines[0:2*nr])    
+    lines_sorted = sort_lines(lines)
+    for i in range(nr):
+        chair1[2*i:2*i+2,:] = lines_sorted[4*i:4*i+2,:]
+        chair2[2*i:2*i+2,:] = lines_sorted[4*i+2:4*i+4,:]
+    return chair1,chair2
+
+###############################################################################
+'''
+finds two crosshair in image 'im' and returns their center coordinates with HLT
+INPUT
+im: nxmx3 array, RGB image
+OUTPUT
+fit_lines: 8x2 array of lines in Hesse normal form (Radius, Theta), sorted by
+theta, starting with smallest theta
+(cx1,cy1): xy center coordinates of crosshair 1
+(cx1,cy1): xy center coordiantes of crosshair 2 
+'''
+###############################################################################
+def find2Xhair(im):
+    im_thres = 128 #luminance threshold
+    edges = rgb2edges_Canny(im,im_thres)
+    # find crosshairs ########################################
+    r_res = 0.5 #radial resolution
+    theta_res = np.pi/3000 #angular resolution
+    v_threshold = 300 #number of votes to be considered 
+    lines = do_HoughLines(edges,r_res,theta_res,v_threshold) 
+    # find fittest lines ####################################
+    number_of_lines = 8 #4 for single crosshair / 8 for two crosshairs
+    space = 10 #pixels (10>= for thick crosshair)
+    ds=20
+    fit_lines = find_fittest_lines4(lines,number_of_lines,ds,im)#only pick the n best fitting lines (unsorted)
+    chair1,chair2=separate_crosshairs(fit_lines)
+    # find center ####################################
+    cx1,cy1 = find_center(chair1) 
+    cx2,cy2 = find_center(chair2) 
+    return fit_lines,(cx1,cy1),(cx2,cy2)
+    
